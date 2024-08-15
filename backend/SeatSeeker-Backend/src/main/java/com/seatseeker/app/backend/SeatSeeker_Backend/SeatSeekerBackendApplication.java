a58d24084d3281fc61c0dc0b2a799d653d8fbe5f
himanshu <kumarehimanshu1@gmail.com>
2024-08-15 10:18:03 +0530

    added backend directory

---
+++ backend/SeatSeeker-Backend/src/main/java/com/seatseeker/app/backend/SeatSeeker_Backend/SeatSeekerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.seatseeker.app.backend.SeatSeeker_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatSeekerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeatSeekerBackendApplication.class, args);
+	}
+
+}
---
+++ backend/SeatSeeker-Backend/src/test/java/com/seatseeker/app/backend/SeatSeeker_Backend/SeatSeekerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seatseeker.app.backend.SeatSeeker_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatSeekerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
