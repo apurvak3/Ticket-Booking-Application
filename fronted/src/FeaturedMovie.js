fad2a29320cefa4d9c840cfd47b76259cfbde710
Harshit279096 <hv279096@gmail.com>
2024-08-25 20:23:45 +0530

    message

--- fronted/src/FeaturedMovie.js
+++ fronted/src/FeaturedMovie.js
@@ -3,7 +3,7 @@ import './FeaturedMovies.css';
 
 const featuredMovies = [
   { id: 1, title: 'Stree 2', image: 'https://images.hindustantimes.com/img/2024/08/15/550x309/stree_2_review_shraddha_kapoor_rajkummar_rao_1723692734817_1723692735009.jpg' },
-  { id: 2, title: 'Kalki', image: 'https://images.hindustantimes.com/img/2024/08/17/1600x900/Kalki_2898_AD_OTT_release_1723868283514_1723868283753.jpg' },
+  { id: 2, title: 'Vedaa', image:'https://cdn.123telugu.com/content/wp-content/uploads/2024/08/vedaa-m.jpg' },
   { id: 3, title: 'Khel khel mein', image: 'https://cinenil.com/wp-content/uploads/2024/07/Khel-Khel-Mein.webp' },
  
 ];
