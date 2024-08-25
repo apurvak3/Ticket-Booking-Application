27ad95192f345264b87bc14ad9eefc6f74d52203
Harshit279096 <hv279096@gmail.com>
2024-08-25 20:23:45 +0530

    message

--- fronted/src/MovieSlider.js
+++ fronted/src/MovieSlider.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 const movies = [
-    { id: 1, title: 'Kalki', image: 'https://images.hindustantimes.com/img/2024/08/17/1600x900/Kalki_2898_AD_OTT_release_1723868283514_1723868283753.jpg'},
+    { id: 1, title: 'Vedaa', image: 'https://cdn.123telugu.com/content/wp-content/uploads/2024/08/vedaa-m.jpg'},
     { id: 2, title: 'Khel Khel Mein', image: 'https://cinenil.com/wp-content/uploads/2024/07/Khel-Khel-Mein.webp' },
     { id: 3, title: 'Stree 2', image: 'https://images.hindustantimes.com/img/2024/08/15/550x309/stree_2_review_shraddha_kapoor_rajkummar_rao_1723692734817_1723692735009.jpg' },
   ];
