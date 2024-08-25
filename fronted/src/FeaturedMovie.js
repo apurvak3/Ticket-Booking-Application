fa5c241cbf3dea24278177dd15a263cf67a4fbd8
Harshit279096 <hv279096@gmail.com>
2024-08-25 22:19:58 +0530

    message

--- fronted/src/FeaturedMovie.js
+++ fronted/src/FeaturedMovie.js
@@ -5,7 +5,7 @@ const featuredMovies = [
   { id: 1, title: 'Stree 2', image: 'https://images.hindustantimes.com/img/2024/08/15/550x309/stree_2_review_shraddha_kapoor_rajkummar_rao_1723692734817_1723692735009.jpg' },
   { id: 2, title: 'Vedaa', image:'https://cdn.123telugu.com/content/wp-content/uploads/2024/08/vedaa-m.jpg' },
   { id: 3, title: 'Khel khel mein', image: 'https://cinenil.com/wp-content/uploads/2024/07/Khel-Khel-Mein.webp' },
- 
+  { id: 3, title: 'Alein:Romulus', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSR9aWtS7Qi2j90FnmtfNr3xJjakAXxvwZulA&s' },
 ];
 
 function FeaturedMovies() {
