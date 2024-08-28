2796dff86676724f3985a503b507a5dd858c079c
Lokendra Dhakrey <lokysthakur123@gmail.com>
2024-08-28 08:26:12 +0530

    Added new front-end project

---
+++ fronted/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./header/Header";
+import Slider from "./carousel/Slider";
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <Slider />
+    </>
+  );
+}
+
+export default Home;
---
+++ fronted/src/components/header/Navbar.jsx
@@ -0,0 +1,78 @@
+import {
+  Disclosure,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+} from "@headlessui/react";
+import { UserCircleIcon } from "@heroicons/react/24/outline";
+
+// const navigation = [
+//   { name: 'Team', href: '#', current: false },
+//   { name: 'Projects', href: '#', current: false },
+//   { name: 'Calendar', href: '#', current: false },
+// ]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function Navbar() {
+  return (
+    <Disclosure as="nav" className="bg-transparent ">
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="relative flex h-16 items-center justify-between">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden"></div>
+          <div className="flex flex-1 items-center justify-start sm:items-stretch sm:justify-start">
+            <div className="flex flex-shrink-0 items-center">
+              <h1 className="h-8 text-3xl font-extrabold font-serif w-auto text-white">
+                FoX
+              </h1>
+            </div>
+          </div>
+          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            {/* Profile dropdown */}
+            <Menu as="div" className="relative ml-3">
+              <div>
+                <MenuButton className="relative flex rounded-full bg-white text-sm focus:outline-none focus:ring-2">
+                  <span className="absolute -inset-1.5" />
+                  <span className="sr-only">Open user menu</span>
+                  <UserCircleIcon className="h-10 w-10 rounded-full"></UserCircleIcon>
+                </MenuButton>
+              </div>
+              <MenuItems
+                transition
+                className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+              >
+                <MenuItem>
+                  <a
+                    href="#"
+                    className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
+                  >
+                    Your Profile
+                  </a>
+                </MenuItem>
+                <MenuItem>
+                  <a
+                    href="#"
+                    className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
+                  >
+                    Settings
+                  </a>
+                </MenuItem>
+                <MenuItem>
+                  <a
+                    href="#"
+                    className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
+                  >
+                    Sign out
+                  </a>
+                </MenuItem>
+              </MenuItems>
+            </Menu>
+          </div>
+        </div>
+      </div>
+    </Disclosure>
+  );
+}
