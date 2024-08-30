14c0c4005d0efa232de52c81be38a10f5ad7b380
Apurva <117301920+apurvak3@users.noreply.github.com>
2024-08-30 21:37:44 +0530

    modified

--- Frontend/src/components/header/SignUp.jsx
+++ Frontend/src/components/header/SignUp.jsx
@@ -96,9 +96,7 @@ const SignUp = () => { // Rename component to SignUp
           Continue with Email
         </button>
       </form>
-      <div className="mt-4 text-center">
-        <a href="#" className="text-sm text-pink-600 hover:text-pink-500">You can also continue with Enterprise SAML SSO</a>
-      </div>
+      
       <div className="mt-6 text-center">
         <p className="text-sm text-gray-600">
           Already have an account? <a href="#" className="font-medium text-pink-600 hover:text-pink-500">Sign In</a>
