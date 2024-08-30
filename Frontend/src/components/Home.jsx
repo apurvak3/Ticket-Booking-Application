35573936529c853ba817dcf10d021371d41b8bf1
Apurva <117301920+apurvak3@users.noreply.github.com>
2024-08-30 08:09:43 +0530

    added seat Booking

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -3,12 +3,14 @@ import Header from "./header/Header";
 import CurrentPlaying from "./movies/currentPlaying";
 import ComingSoon from "./movies/ComingSoon";
 import LoginPage from "./header/LoginPage";
+import SeatBooking from "./SeatBooking/SeatBooking";
 
 function Home() {
   return (
     <>
       <Header />
       <LoginPage/>
+      <SeatBooking/>
       <CurrentPlaying/>
       <ComingSoon/>
       
