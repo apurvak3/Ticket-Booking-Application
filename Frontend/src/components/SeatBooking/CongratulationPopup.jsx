9c4eae7c43dde2cd50774bf4ecf5557342ec44ff
Lokendra Dhakrey <lokysthakur123@gmail.com>
2024-08-31 01:19:00 +0530

    Fixed

--- Frontend/src/components/SeatBooking/CongratulationPopup.jsx
+++ Frontend/src/components/SeatBooking/CongratulationPopup.jsx
@@ -32,7 +32,7 @@ export default function CongratulationPopup() {
                   <div className="mt-2">
                     <p className="text-sm text-gray-500">
                       Are you sure you want to deactivate your account? All of your data will be permanently removed.
-                      This action cannot be undone.
+                       This action cannot be undone.
                     </p>
                   </div>
                 </div>
